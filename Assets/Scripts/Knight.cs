5150e67bef76556e9954bd6b9c05ecb4d3fb267a
Blanc35 <119550267+Blanc35@users.noreply.github.com>
2023-12-19 20:29:57 +0800

    k

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -43,4 +43,20 @@ public class Knight : chess
         return false;
 
     }
+           public override List<Vector2Int> getMoveable(Vector2Int grid2)
+    {
+        List<Vector2Int> moveable = new List<Vector2Int>();
+
+        int[][] directions = knight;
+        for(int dir = 0; dir < directions.GetLength(0); dir++)
+        {
+            for (int i = 1; i < 2; i++)
+            {
+                Vector2Int moveableGrid2 = new Vector2Int(grid2.x + i * directions[dir][0], grid2.y + i * directions[dir][1]);
+                moveable.Add(moveableGrid2);
+            }
+        }
+
+        return moveable;
+    }
 }
