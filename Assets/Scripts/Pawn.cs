3e02b4360d1a01e82c6dcc78eea58a9adcba78b9
Blanc35 <119550267+Blanc35@users.noreply.github.com>
2024-01-02 20:48:02 +0800

    iji

--- Assets/Scripts/Pawn.cs
+++ Assets/Scripts/Pawn.cs
@@ -8,6 +8,8 @@ public class Pawn : chess
 {
         new int[]{0,1},
         new int[]{0,2},
+         new int[]{1,1},
+          new int[]{-1,1},
     };
     // Start is called before the first frame update
     void Start()
@@ -59,22 +61,55 @@ public class Pawn : chess
 
         int[][] directions = pawn;
         int dirLength = (Gamedev.instance.determineMove(this) ? 1 : 2);
-        for(int dir = 0; dir < dirLength; dir++)
+        for (int dir = 0; dir < dirLength; dir++)
         {
             for (int i = 1; i < 2; i++)
             {
-                if(mChesspPieces== chesspPieces.Black)
+                if (mChesspPieces == chesspPieces.Black)
                 {
-                    Vector2Int moveableGrid2 = new Vector2Int(grid2.x + i* directions[dir][0], grid2.y + i * -1 * directions[dir][1]);
-                    moveable.Add(moveableGrid2);
+                    Vector2Int moveableGrid2 = new Vector2Int(grid2.x + i * directions[dir][0], grid2.y + i * -1 * directions[dir][1]);
+                    if (Gamedev.instance.ba.getChess(moveableGrid2) == null)
+                    {
+                        moveable.Add(moveableGrid2);
+                    }
                 }
                 else
                 {
                     Vector2Int moveableGrid2 = new Vector2Int(grid2.x + i * directions[dir][0], grid2.y + i * directions[dir][1]);
-                    moveable.Add(moveableGrid2);
+                    if (Gamedev.instance.ba.getChess(moveableGrid2) == null)
+                    {
+                        moveable.Add(moveableGrid2);
+                    }
                 }
             }
         }
+
+
+        for (int i = 2; i < 4; i++)
+        {
+
+            if (mChesspPieces == chesspPieces.Black)
+            {
+
+                Vector2Int moveableGrid2 = new Vector2Int(grid2.x + directions[i][0], grid2.y + -1 * directions[i][1]);
+                if (!Gamedev.instance.isFriendlyChess(moveableGrid2) && Gamedev.instance.ba.getChess(moveableGrid2) != null)
+                {
+                    moveable.Add(moveableGrid2);
+                }
+
+            }
+            else
+            {
+                Vector2Int moveableGrid2 = new Vector2Int(grid2.x + directions[i][0], grid2.y + directions[i][1]);
+                if (!Gamedev.instance.isFriendlyChess(moveableGrid2) && Gamedev.instance.ba.getChess(moveableGrid2) != null)
+                {
+                    moveable.Add(moveableGrid2);
+                }
+            }
+
+        }
+
+
 
         return moveable;
     }
